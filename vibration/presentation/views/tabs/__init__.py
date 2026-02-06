cfa4c07760731f7988801797e283e349ab24e4cf
siruharu <hotman7025@naver.com>
2026-02-06 09:44:36 +0900

    refactor(tabs): extract TrendTabView from monolith

--- vibration/presentation/views/tabs/__init__.py
+++ vibration/presentation/views/tabs/__init__.py
@@ -1,4 +1,5 @@
 """Tab view components."""
 from .spectrum_tab import SpectrumTabView
+from .trend_tab import TrendTabView
 
-__all__ = ['SpectrumTabView']
+__all__ = ['SpectrumTabView', 'TrendTabView']

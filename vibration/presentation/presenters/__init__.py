d651d8979fdd9a8d06c260dae467b4516e37dd7f
siruharu <hotman7025@naver.com>
2026-02-06 09:48:15 +0900

    refactor(presenters): create TrendPresenter with DI

--- vibration/presentation/presenters/__init__.py
+++ vibration/presentation/presenters/__init__.py
@@ -1,4 +1,5 @@
 """Presenter components (MVP pattern)."""
 from .spectrum_presenter import SpectrumPresenter
+from .trend_presenter import TrendPresenter
 
-__all__ = ['SpectrumPresenter']
+__all__ = ['SpectrumPresenter', 'TrendPresenter']
